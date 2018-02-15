900cde66034668419ab44431f17960208592c225
Fletcher Morris <odmoandtheboy@gmail.com>
2018-02-15 02:38:31 +0000

    Added random number generation

--- Arctic-Engine/main.cpp
+++ Arctic-Engine/main.cpp
@@ -1,9 +1,16 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
-
 #include "Game.h"
+#include "util/Random.h"
 
 int main()
 {
+    Random<> random;
+
+    for(int i = 0; i < 30; i++){
+        std::cout << random.Int(0, 10) << "\n";
+    }
+
     Game game;
     game.Run();
 
