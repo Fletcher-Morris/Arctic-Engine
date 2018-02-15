b6e5bac521926a639c6574c725c22ba2a32126c7
Fletcher Morris <odmoandtheboy@gmail.com>
2018-02-15 02:14:46 +0000

    Added some time calculation maths

--- Arctic-Engine/src/Game.cpp
+++ Arctic-Engine/src/Game.cpp
@@ -14,15 +14,39 @@ void Game::Run()
 {
 
     sf::Clock clock;
+    constexpr unsigned  TICKS_PER_SECOND = 60;
+    const sf::Time      TICKS_PER_UPDATE = sf::seconds(1.0f / float(TICKS_PER_SECOND));
+    unsigned    ticks = 0;
+
+    sf::Clock timer;
+    auto lastTime = sf::Time::Zero;
+    auto lag      = sf::Time::Zero;
+
     while (m_window.isOpen() && !m_states.empty())
     {
-        auto dt = clock.restart();
         auto& state = GetCurrentState();
+
+
+        auto time = timer.getElapsedTime();
+        auto elapsed = time - lastTime;
+        lastTime = time;
+        lag += elapsed;
+
+        //  Delta Time
         state.HandleInput();
-        state.Update(dt);
+        state.Update(elapsed);
+        //  Fixed Time
+        while(lag >= TICKS_PER_UPDATE)
+        {
+            ticks++;
+            lag -= TICKS_PER_UPDATE;
+            state.FixedUpdate(elapsed);
+        }
+        //  Render
         m_window.clear();
         state.Render(m_window);
         m_window.display();
+        //  Events
         HandleEvents();
         TryPop();
     }
