4e4570898e32dc11db6ebd6a1ebc7c0e9a07f53b
Fletcher Morris <odmoandtheboy@gmail.com>
2018-02-18 17:56:50 +0000

    Implemented the 'CreateModelMatrix()' method

--- Arctic-Engine/source/render/MatrixMaths.cpp
+++ Arctic-Engine/source/render/MatrixMaths.cpp
@@ -1,8 +1,17 @@
 #include "MatrixMaths.h"
+#include "../entity/Entity.h"
+#include "../util/GlmTypedefs.h"
 
 Matrix4 CreateModelMatrix(const Entity & entity)
 {
-	return Matrix4();
+	Matrix4 matrix;
+
+	matrix = glm::rotate(matrix, glm::radians(entity.rotation.x), { 1,0,0 });
+	matrix = glm::rotate(matrix, glm::radians(entity.rotation.y), { 0,1,0 });
+	matrix = glm::rotate(matrix, glm::radians(entity.rotation.z), { 0,0,1 });
+	matrix = glm::translate(matrix, entity.position);
+
+	return matrix;
 }
 
 Matrix4 CreateViewMatrix(const Camera & camera)
