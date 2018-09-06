711e621d5ae4521a9aa7581497224e38fff38e05
Fletcher Morris <odmoandtheboy@gmail.com>
2018-09-06 12:28:20 +0100

    Made transform class prettier

--- Arctic-Engine/source/entity/Transform.h
+++ Arctic-Engine/source/entity/Transform.h
@@ -8,10 +8,10 @@ public:
 	Vector3 rotation;
 	Vector3 scale;
 
-	const Vector3 up =		{ 0, 1, 0 };
-	const Vector3 down =	{ 0,-1, 0 };
-	const Vector3 right =	{ 1, 0, 0 };
-	const Vector3 left =	{-1, 0, 0 };
-	const Vector3 forward =	{ 0, 0, 1 };
-	const Vector3 back =	{ 0, 0,-1 };
+	const Vector3 up		=	{ 0, 1, 0 };
+	const Vector3 down		=	{ 0,-1, 0 };
+	const Vector3 right		=	{ 1, 0, 0 };
+	const Vector3 left		=	{-1, 0, 0 };
+	const Vector3 forward	=	{ 0, 0, 1 };
+	const Vector3 back		=	{ 0, 0,-1 };
 };
