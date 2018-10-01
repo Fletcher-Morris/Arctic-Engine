d4d0f72abdc61855dc2f21b498e706f24fc75973
Fletcher Morris <odmoandtheboy@gmail.com>
2018-10-01 03:39:18 +0100

    'State_Editor' now inherits from 'State' class

---
+++ Arctic-Engine/source/states/State_Editor.cpp
@@ -0,0 +1,25 @@
+#include "State_Editor.h"
+
+void State_Editor::HandleEvent(int _event)
+{
+}
+
+void State_Editor::HandleInput(GLFWwindow * _window)
+{
+}
+
+void State_Editor::Update(double _deltaTime)
+{
+}
+
+void State_Editor::FixedUpdate(double _fixedTime)
+{
+}
+
+void State_Editor::GuiUpdate()
+{
+}
+
+void State_Editor::Render(GLFWwindow * _target)
+{
+}
--- Arctic-Engine/source/states/State_Editor.h
+++ Arctic-Engine/source/states/State_Editor.h
@@ -1,8 +1,17 @@
 #pragma once
-class State_Editor
+#include "State.h"
+
+class State_Editor : public State
 {
 public:
 	State_Editor();
 	~State_Editor();
-};
 
+	// Inherited via State
+	virtual void HandleEvent	(int _event)			override;
+	virtual void HandleInput	(GLFWwindow * _window)	override;
+	virtual void Update			(double _deltaTime)		override;
+	virtual void FixedUpdate	(double _fixedTime)		override;
+	virtual void GuiUpdate		()						override;
+	virtual void Render			(GLFWwindow * _target)	override;
+};
